06cfc25ea457a47867353e541dfb4110b97766d9
ClementPac <clement.pacault@gmail.com>
2025-02-04 14:58:15 +0100

    deplacement de controlGeneralFrame de mainFrame.py vers mytools.py et update du readme pour controlGeneralFrame et AxisButtonsFrame

--- README.md
+++ README.md
@@ -7,6 +7,9 @@
 - SettingsLabeledEntry : lbl+combobox+disable entry+unit
 - SettingsFrame
 - ControlFrame : to go from absolute to increment movement for example
+- AxisButtons : two buttons, with texts + and - used for an axis.
+- AxisButtonsFrame : display buttons for each axis defined, layout from 1 axis to 3 (easily upgradable).
+- ControlGeneralFrame : display current axis values, buttons to stop, set as zero and go to zero
 
 ### Usefull Functions/Methods
 - searchByName : search elem from dictionary by attribute "name" and return the element with all other attributes.
--- mytools.py
+++ mytools.py
@@ -521,6 +521,107 @@ class AxisButtonsFrame(tk.Frame):
             child.pack_forget()
             child.grid_forget()
 
+class ControlGeneralFrame(tk.Frame):
+    """
+    Summary:
+        Frame that display axis values, with 3 buttons to do main actions quickly.
+        Contain a button Stop, Set to zero and Go to zero plus labels for axis current values.
+    Parameters:
+        - btnStop : button, labeled "Stop movements", meant to send a stop signal.
+        - btnSetZero : button, labeled "Set as zero", meant to set current axis values to zero.
+        - btnGoZero : button, labeled "Go to zero", meant send signals to get the current axis values to zero.
+        - lblNames : list of Label, axis names in front of their current values.
+        - inpAxisValues : dict of axis_names:DoubleVar, inputs containing current axis values.
+        - lblAxisValues : list of Label, labels displaying inpAxisValues values.
+        - callbacks : dict of btnName:list of functions, attribute callbacks to buttons of the frame.
+    """
+    STOP = "stop"
+    SETZERO = "setzero"
+    GOZERO = "gozero"
+
+    def __init__(self, master, axis_names, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+
+        self.btnStop    = tk.Button(self, text="STOP Movements",command=self.stop)
+        self.btnSetZero = tk.Button(self, text="Set as Zero",   command=self.setZero)
+        self.btnGoZero  = tk.Button(self, text="Go to Zero",    command=self.goZero)
+
+        self.btnStop.config(
+            height=1, 
+            font=("Arial",13), 
+            bg="#E70606", 
+            fg="#FFFFFF", 
+            relief="solid", 
+            borderwidth=2,
+            activebackground="#B50000",
+            activeforeground="#DDDDDD")
+        self.btnSetZero.config(
+            height=1, 
+            font=("Arial",12), 
+            bg="#C06C55", 
+            fg="#FFFFFF", 
+            relief="flat",
+            activebackground="#964D39",
+            activeforeground="#DDDDDD")
+        self.btnGoZero.config(
+            height=1, 
+            font=("Arial",12), 
+            bg="#CBB46A", 
+            fg="#FFFFFF", 
+            relief="flat",
+            activebackground="#A89249",
+            activeforeground="#DDDDDD")
+
+        self.lblNames = [ tk.Label(self, text=axis_name) for axis_name in axis_names ]
+        self.inpAxisValues = { axis_name:tk.DoubleVar() for axis_name in axis_names }
+        self.lblAxisValues = [ tk.Label(self, textvariable=inpAxis, bg="#B5D6C1", anchor="w") for inpAxis in self.inpAxisValues.values() ]
+
+        self.callbacks = {
+            "stop": [],
+            "setzero": [],
+            "gozero": []
+        }
+
+        self.apply_layout()
+
+    def addCallback(self, target: str, callback):
+        self.callbacks[target].append(callback)
+
+    def stop(self):
+        # call callbacks
+        for cb in self.callbacks[self.STOP]: cb()
+    
+    def setZero(self):
+        # call callbacks
+        for cb in self.callbacks[self.SETZERO]: cb()
+
+    def goZero(self):
+        # call callbacks
+        for cb in self.callbacks[self.GOZERO]: cb()
+
+    def apply_layout(self):
+        self.pack(fill="x")
+        self.rowconfigure(tuple(range(len(self.lblNames)+3)), weight=3, uniform='a')
+        self.rowconfigure((0), weight=4, uniform='a')
+        self.columnconfigure((0), weight=1, uniform='a')
+        self.columnconfigure((1), weight=3, uniform='a')
+
+        self.btnStop.   grid(row=0, column=0, columnspan=2, sticky="nsew", pady=2)
+        self.btnSetZero.grid(row=4, column=0, columnspan=2, sticky="ew", pady=2)
+        self.btnGoZero. grid(row=5, column=0, columnspan=2, sticky="ew", pady=2)
+        
+        for idxAxis in range(len(self.lblNames)):
+            self.lblNames[idxAxis].     grid(row=idxAxis+1, column=0, sticky="ew")
+            self.lblAxisValues[idxAxis].grid(row=idxAxis+1, column=1, sticky="ew", padx=10)
+
+    def reset_layout(self):
+        self.pack_forget()
+        self.grid_forget()
+
+        for child in self.winfo_children():
+            child.pack_forget()
+            child.grid_forget()
+
 
 ## --------------------------------------
 ## USEFULL FUNCTIONS
@@ -680,6 +781,14 @@ def testAxisButtonsFrame():
 
     root.mainloop()
 
+def testControlGeneralFrame():
+    print("start testControlGeneralFrame")
+    root = tk.Tk()
+
+    cgf = ControlGeneralFrame(root, ('X','Y','Z'))
+
+    root.mainloop()
+
 def testDict():
     dico = {
         "platine1": {
@@ -703,6 +812,7 @@ if __name__ == "__main__":
     # testAxisFrame()
     # testSettingsFrame()
     # testControlFrame()
-    testAxisButtonsFrame()
+    # testAxisButtonsFrame()
+    testControlGeneralFrame()
 
     print("end")
