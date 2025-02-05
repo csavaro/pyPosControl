ea8608d3126c89b0bae3462c67ae059a9268b790
ClementPac <clement.pacault@gmail.com>
2025-02-05 16:26:58 +0100

    debut creation classe CSeries

--- communications.py
+++ communications.py
@@ -14,3 +14,16 @@ class Commands(ABC):
     def moveCmd(self):
         pass
 
+class CSeries(Commands):
+    def __init__(self, axis_values: dict, axis_speeds: dict):
+        super().__init__(axis_values, axis_speeds)
+
+    def stopCmd(self):
+        return "@0R3\n\r".encode(encoding="ascii")
+    
+    def moveCmd(self):
+        # maybe add axis definition command in return
+        axisStr = ""
+        for axis,dist in self.values.items():
+            axisStr+=f"{dist},{self.speeds[axis]}," 
+        return f"@0a {axisStr[:-1]}\n\r".encode("ascii")
